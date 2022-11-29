c8c1d137a6056f423697c9458f29b8db0c3244f0
mor daphna <mordafna33@gmail.com>
2022-11-29 13:15:05 +0200

    add-supabase

--- config.js
+++ config.js
@@ -1,11 +1,22 @@
 // Use this for configuration settings in                                               
 export const API_BASE = "https://www.balldontlie.io/api/v1/teams";
 
-// export const API_ENDPOINT = `${API_BASE}/?q=teams`;
+
+
+// export const SUPABASE_URL= process.env.SUPABASE_URL;
+// export const SUPABASE_SERVICE_KEY= process.env.SUPABASE_SERVICE_KEY;
+// export const CONTACT_FORM_TABLE_NAME = "afl_contact_submissions";
+// export const CONTACT_FORM_ENDPOINT = "/api/contact/";
+
+export const SUPABASE_URL = process.env.SUPABASE_URL;
+export const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
+export const CONTACT_FORM_ENDPOINT = "/api/contact/";
+export const CONTACT_FORM_TABLE_NAME = "afl_contact_submissions";
+
 
 // NOTE: the common approach is to set these values from environment variables
 // and not to have the values "hard-coded" with the code.
 // Then, you use the host or wherever the process is running to set the variables
 // and you read them in here
 // So if you do that it will look something like:
-// export const API_BASE = process.env.API_BASE
+//export const API_BASE = process.env.API_BASE
--- pages/contact.js
+++ pages/contact.js
@@ -1,45 +1,91 @@
 import { useState } from "react";
+import { CONTACT_FROM_TABLE_NAME } from "../config";
+//the endpoint we need to make a post request to add the comment info
+import { CONTACT_FORM_ENDPOINT } from "../config";
 
+//watched this video https://www.youtube.com/watch?v=EcRFYF4B3IQ
+async function postContactForm(data){
+  const response = await fetch(CONTACT_FORM_ENDPOINT, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data),
+  });
+  return response;
+
+}
 export default function ContactPage() {
-
+  //handle the change of state when the fields name,email and comment are changing 
   const [formData, setFormData] = useState({});
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log(formData);
-    alert("Not yet handling form submission");
-  }
+  const [formResponseStatus, setFormResponseStatus] = useState();
 
+  async function handleSubmit(event) {
+    //prevent the page from being refreshed when submmiting 
+    event.preventDefault();
+    const response = await postContactForm(formData);
+    //check status for succsess or errors
+    // console.log(formData)
+    setFormResponseStatus(response.status);
+}
+
+
+  //when each one of name email or comment is being change we save it in the an object and update it will the user typing
+  //https://www.youtube.com/watch?v=IkMND33x0qQ
   function handleChange(event) {
     if (event.target.id === "form-name") {
       const name = event.target.value;
-      setFormData(Object.assign(formData, { name }))
+      setFormData(Object.assign(formData, { name }));
     }
     if (event.target.id === "form-email") {
       const email = event.target.value;
-      setFormData(Object.assign(formData, { email }))
+      setFormData(Object.assign(formData, { email }));
     }
     if (event.target.id === "form-comment") {
       const comment = event.target.value;
-      setFormData(Object.assign(formData, { comment }))
+      setFormData(Object.assign(formData, { comment }));
     }
   }
 
   return (
     <div className="row">
       <div className="item">
+
         <div className="content">
           Contact us if you found any mistakes or missing data, or if you just want to say hello!
         </div>
+
         <div className="content">
-          <form onSubmit={handleSubmit}>
-            <input id="form-name" placeholder="Your name" value={formData.name} onChange={handleChange} required />
-            <input id="form-email" type="email" placeholder="Your email" value={formData.email} onChange={handleChange} required />
-            <textarea id="form-comment" placeholder="Your comment" value={formData.comment} onChange={handleChange}></textarea>
-            <button type="submit">Send</button>
-          </form>
+
+          {formResponseStatus ?(
+            <div>{formResponseStatus === 200 ? "Thank you." : "Something went wrong."}</div> ):(
+            <form onSubmit={handleSubmit}>
+              <input id="form-name"
+                      placeholder="Your name"
+                      //when we are trying to change from data.name we will call handleChange to change ftomdata
+                      value={formData.name} 
+                      onChange={handleChange} 
+              required />
+              <input id="form-email"
+                      type="email"
+                      placeholder="Your email" 
+                      value={formData.email} 
+                      onChange={handleChange} 
+               required />
+              <textarea id="form-comment"
+                        placeholder="Your comment" 
+                        value={formData.comment} 
+                        onChange={handleChange}>
+              </textarea>
+              <button type="submit">Send</button>
+
+            </form>
+          )}
         </div>
+
       </div>
     </div >
-  )
+  );
 }
+
